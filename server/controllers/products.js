49450dbbeefd108f456733bdb9853c4bdaaf23da
JordyCheco <112654260+JordyCheco@users.noreply.github.com>
2022-11-28 15:17:55 -0500

    implementing MongoDB and fixing code

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -4,6 +4,17 @@ const products = require('../models/products');
 const app = express.Router();
 
 app
+    .get('/', (req, res, next) => {
+        products.getProducts()
+            .then(x => res.status(200).send(x))
+            .catch(next);
+    })
+    .get('/', (req, res, next) => {
+        products.getProduct()
+            .then(product => res.status(200).send(product))
+            .catch(next);
+    })
+
     .get('/', (req, res) => {
         res.status(200).send(products.getProducts());
     })
