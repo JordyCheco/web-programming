ab894232e9e22b904b8638b0a4a576e858e5fb9b
JordyCheco <112654260+JordyCheco@users.noreply.github.com>
2022-11-29 19:46:53 -0500

    fixed cart issues

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,21 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import 'bulma'
-
-import ttt, {add, subtract} from './stores/temp';
-console.log(ttt);
-const bigger = add(ttt, 2);
-console.log(bigger);
-const reducer =2;
-if(reducer > 0)
-    {
-    const smaller = subtract(ttt, reducer);
-    console.log(smaller);
-    }
 
 import './assets/main.scss'
-
 
 const app = createApp(App)
 
